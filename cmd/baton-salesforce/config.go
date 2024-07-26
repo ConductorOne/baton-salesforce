849102d44eb4fb85ac43fd7999d7898d6561cb3c
Marcos Gaeta <mgaeta89@gmail.com>
2024-07-26 15:28:21 -0700

    WIP configs

--- cmd/baton-salesforce/config.go
+++ cmd/baton-salesforce/config.go
@@ -1,17 +1,28 @@
 package main
 
 import (
-	"context"
-
-	"github.com/conductorone/baton-sdk/pkg/cli"
+	"github.com/conductorone/baton-sdk/pkg/field"
 )
 
-// config defines the external configuration required for the connector to run.
-type config struct {
-	cli.BaseConfig `mapstructure:",squash"` // Puts the base config options in the same place as the connector options
-}
-
-// validateConfig is run after the configuration is loaded, and should return an error if it isn't valid.
-func validateConfig(ctx context.Context, cfg *config) error {
-	return nil
-}
+var (
+	InstanceUrlField = field.StringField(
+		"instance-url",
+		field.WithDescription("Your Salesforce domain, ex: acme.my.salesforce.com"),
+		field.WithRequired(true),
+	)
+	UseUsernameForEmailField = field.BoolField(
+		"user-username-for-email",
+		field.WithDescription("Use Salesforce usernames for email"),
+	)
+	TODOTokenField = field.StringField(
+		"todo-token",
+		field.WithDescription("TODO MARCOS FIRST OAUTH"),
+		field.WithRequired(true),
+	)
+	configurationFields = []field.SchemaField{
+		InstanceUrlField,
+		UseUsernameForEmailField,
+		TODOTokenField,
+	}
+	configuration = field.NewConfiguration(configurationFields)
+)
--- pkg/connector/connector.go
+++ pkg/connector/connector.go
@@ -2,43 +2,105 @@ package connector
 
 import (
 	"context"
+	"fmt"
 	"io"
+	"net/url"
 
+	"github.com/conductorone/baton-salesforce/pkg/connector/client"
 	v2 "github.com/conductorone/baton-sdk/pb/c1/connector/v2"
 	"github.com/conductorone/baton-sdk/pkg/annotations"
 	"github.com/conductorone/baton-sdk/pkg/connectorbuilder"
 )
 
-type Connector struct{}
+func annotationsForUserResourceType() annotations.Annotations {
+	annos := annotations.Annotations{}
+	annos.Update(&v2.SkipEntitlementsAndGrants{})
+	return annos
+}
 
-// ResourceSyncers returns a ResourceSyncer for each resource type that should be synced from the upstream service.
-func (d *Connector) ResourceSyncers(ctx context.Context) []connectorbuilder.ResourceSyncer {
+type Salesforce struct {
+	client                    *client.SalesforceClient
+	shouldUseUsernameForEmail bool
+	// TODO MARCOS 1.3
+	// tokenSource               oauth2.TokenSource
+}
+
+// fallBackToHTTPS checks to domain and tacks on "https://" if no scheme is
+// specified. This exists so that a user can override the scheme by including it
+// in the passed "domain-url" config.
+func fallBackToHTTPS(domain string) (string, error) {
+	parsed, err := url.Parse(domain)
+	if err != nil {
+		return "", err
+	}
+	if parsed.Scheme == "" {
+		parsed, err = url.Parse(fmt.Sprintf("https://%s", domain))
+		if err != nil {
+			return "", err
+		}
+	}
+	return parsed.String(), nil
+}
+
+// ResourceSyncers returns a ResourceSyncer for each resource type that should
+// be synced from the upstream service.
+func (d *Salesforce) ResourceSyncers(ctx context.Context) []connectorbuilder.ResourceSyncer {
 	return []connectorbuilder.ResourceSyncer{
-		newUserBuilder(),
+		newUserBuilder(d.client, d.shouldUseUsernameForEmail),
+		newGroupBuilder(d.client),
+		newPermissionBuilder(d.client),
+		newProfileBuilder(d.client),
+		newRoleBuilder(d.client),
 	}
 }
 
-// Asset takes an input AssetRef and attempts to fetch it using the connector's authenticated http client
-// It streams a response, always starting with a metadata object, following by chunked payloads for the asset.
-func (d *Connector) Asset(ctx context.Context, asset *v2.AssetRef) (string, io.ReadCloser, error) {
+// Asset takes an input AssetRef and attempts to fetch it using the connector's
+// authenticated http client. It streams a response, always starting with a
+// metadata object, following by chunked payloads for the asset.
+func (d *Salesforce) Asset(ctx context.Context, asset *v2.AssetRef) (string, io.ReadCloser, error) {
 	return "", nil, nil
 }
 
 // Metadata returns metadata about the connector.
-func (d *Connector) Metadata(ctx context.Context) (*v2.ConnectorMetadata, error) {
+func (d *Salesforce) Metadata(ctx context.Context) (*v2.ConnectorMetadata, error) {
 	return &v2.ConnectorMetadata{
-		DisplayName: "My Baton Connector",
-		Description: "The template implementation of a baton connector",
+		DisplayName: "Salesforce",
+		Description: "Connector syncing Salesforce users and TODO MARCOS",
 	}, nil
 }
 
-// Validate is called to ensure that the connector is properly configured. It should exercise any API credentials
-// to be sure that they are valid.
-func (d *Connector) Validate(ctx context.Context) (annotations.Annotations, error) {
-	return nil, nil
+// Validate is called to ensure that the connector is properly configured. It
+// should exercise any API credentials to be sure that they are valid.
+func (d *Salesforce) Validate(ctx context.Context) (annotations.Annotations, error) {
+	_, ratelimitData, err := d.client.GetInfo(ctx)
+	outputAnnotations := client.WithRateLimitAnnotations(ratelimitData)
+	return outputAnnotations, err
 }
 
 // New returns a new instance of the connector.
-func New(ctx context.Context) (*Connector, error) {
-	return &Connector{}, nil
+func New(
+	ctx context.Context,
+	instanceURL string,
+	useUsernameForEmail bool,
+	todoToken string,
+) (*Salesforce, error) {
+	instanceURL, err := fallBackToHTTPS(instanceURL)
+	if err != nil {
+		return nil, err
+	}
+
+	salesforceClient, err := client.NewSalesforceClient(
+		ctx,
+		instanceURL,
+		todoToken,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	salesforce := Salesforce{
+		client:                    salesforceClient,
+		shouldUseUsernameForEmail: useUsernameForEmail,
+	}
+	return &salesforce, nil
 }
